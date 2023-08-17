480bdb7305f6020e6e20ee15296e5970d0e8a139
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-08-16 19:48:37 -0500

    Autenticacion

--- src/main/java/com/example/demo/controller/TokenControllerResFul.java
+++ src/main/java/com/example/demo/controller/TokenControllerResFul.java
@@ -14,7 +14,7 @@ import com.example.demo.service.to.UsuarioTo;
 @CrossOrigin
 public class TokenControllerResFul {
 	
-	@GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping("/obtener")
 	public String obtenerToken(@RequestBody UsuarioTo usuario) {
 		
 		return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
